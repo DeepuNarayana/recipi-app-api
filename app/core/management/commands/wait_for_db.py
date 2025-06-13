00045254225e066f6f106ee035293bdc74e0eb3a
deepunarayana <dpu7481@gmail.com>
2025-06-13 14:54:09 +0530

    Made changes in db file and test

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -18,10 +18,9 @@ class Command(BaseCommand):
         while db_up is False:
             try:
                 self.check(databases=['default'])
-                db_up = True
-                
+                db_up = True                
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
-                time.sleep(1)       
-
+                time.sleep(1)
+                
         self.stdout.write(self.style.SUCCESS('Database available!'))
